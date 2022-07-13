4c48cdae830f08bf9f5a64956752b535404eb68a
myoungjinkang <rkdwlss2@gmail.com>
2022-07-13 17:50:34 +0900

    회원가입 로직 수정 socail id 같은거 무한 회원가입 막기... 존재하는 소셜 ID면 500에러 나옴

---
+++ backend/src/main/java/game/mozzi/handler/ex/CustomValidationException.java
@@ -0,0 +1,29 @@
+package game.mozzi.handler.ex;
+
+import java.util.Map;
+
+public class CustomValidationException extends RuntimeException{
+
+
+    // 객체를 구분할때
+    private static final long serialVersionUID =1L;
+
+    //    private String message;
+    private Map<String,String> errorMap;
+
+    public CustomValidationException(String message,Map<String,String> errorMap){
+        super(message); //부모한테 던짐
+        //        this.message=message;
+        this.errorMap=errorMap;
+    }
+
+    public CustomValidationException(String message){
+        super(message); //부모한테 던짐
+    }
+
+    public Map<String,String> getErrorMap(){
+        return errorMap;
+    }
+
+
+}
--- backend/src/main/java/game/mozzi/service/UserService.java
+++ backend/src/main/java/game/mozzi/service/UserService.java
@@ -2,6 +2,7 @@ package game.mozzi.service;
 
 import game.mozzi.domain.user.User;
 import game.mozzi.domain.user.UserInfoRepository;
+import game.mozzi.handler.ex.CustomValidationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,6 +15,10 @@ public class UserService {
 
     @Transactional
     public User join(User user){
+
+        if (userInfoRepository.existsBySocialId(user.getSocialId())){
+            throw new CustomValidationException("이미 존재하는 소셜아이디 입니다");
+        }
         User userEntity = userInfoRepository.save(user);
         return userEntity;
     }
