044044e616369332ba642d18bae5c3dcaff7373e
beomchul.kim <2017253083@yonsei.ac.kr>
2022-07-14 13:56:46 +0900

    메시지 공통처리 , 로그변경

---
+++ backend/src/main/java/game/mozzi/config/response/CommonConstants.java
@@ -0,0 +1,24 @@
+package game.mozzi.config.response;
+
+
+/**
+ * 작성자 : beomchul.kim@lotte.net
+ * - CommonConstants
+ * 규칙 MZ_xx_yyyy
+ * xx : 모듈
+ * yy : 순번
+ * 모듈 = {
+ *     00 : 회원
+ *     01 :
+ *     02 :
+ *     03 :
+ * }
+ */
+public final class CommonConstants {
+
+    public static final String MZ_00_0001 = "로그인";
+    public static final String MZ_00_0002 = "회원가입";
+    public static final String MZ_00_0003 = "회원가입 실패";
+    public static final String MZ_00_0004 = "게스트 로그인";
+
+}
