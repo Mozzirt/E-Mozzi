8783169b53b6eb9e84b3d5ac01919ca1e31af73e
myoungjinkang <rkdwlss2@gmail.com>
2022-07-15 14:16:37 +0900

    [BE] 질문 리스트 추가

--- backend/src/main/java/game/mozzi/domain/question/QuestionList.java
+++ backend/src/main/java/game/mozzi/domain/question/QuestionList.java
@@ -2,6 +2,7 @@ package game.mozzi.domain.question;
 
 import game.mozzi.domain.BaseTimeEntity;
 import game.mozzi.domain.user.User;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -9,7 +10,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import lombok.AllArgsConstructor;
@@ -23,28 +23,40 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 @Entity // DB에 테이블을 생성 해줌
-public class Question extends BaseTimeEntity {
+public class QuestionList extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //번호 증가 전략이 데이터 베이스를 따라 갑니다.
     private Long id; // 서비스 하는 프로그램이 아니라서 int로도 충분함
 
-    @Column(name = "title")
-    private String title;
+    // 질문 리스트 명
+    @Column(name = "name")
+    private String name;
 
+    @OneToMany(mappedBy = "questionList",fetch = FetchType.LAZY)
+    private List<Question> questions;
 
-    @OneToOne
-    @JoinColumn(name = "userId")
-    private User user;
+    @Column(name = "content1")
+    private String content1;
 
-    @Column(name = "password")
-    private String password;
+    @Column(name = "content2")
+    private String content2;
 
-    // 이어하기
-    @Column(name = "continueDo")
-    private String continueDo;
+    @Column(name = "content3")
+    private String content3;
 
-    @JoinColumn(name = "questionlistId")
-    @ManyToOne
-    private QuestionList questionList;
+    @Column(name = "content4")
+    private String content4;
+
+    @Column(name = "content5")
+    private String content5;
+
+    @Column(name = "content6")
+    private String content6;
+
+    @Column(name = "content7")
+    private String content7;
+
+    @Column(name = "picture")
+    private String picture;
 
 }
