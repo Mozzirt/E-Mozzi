b6317c7fb913b60dd39dd4cea5d2a88e6615f368
joohwanjeon <joohwan.jeon@kyobobook.com>
2022-07-15 15:22:10 +0900

    [FE] 로그인 페이지 임시 로고 추가

--- frontend/src/page/Login.js
+++ frontend/src/page/Login.js
@@ -1,4 +1,4 @@
-import { Box, color, Flex, Text } from "@chakra-ui/react"
+import { Box, Flex, Text, Image } from "@chakra-ui/react"
 
 
 
@@ -6,20 +6,19 @@ import { Box, color, Flex, Text } from "@chakra-ui/react"
 export default function Login() {
     return (
         <Box align="center">
-            <Flex align="center" justify="center" h="200px" bg="gray.100">
-                <Text>
-                    메인 로고 영역
-                </Text>
+            <Flex align="center" justify="center" h="400px" bg="gray.100">
+                <Image src="image/logo_main.svg" alt="main logo"></Image>
             </Flex>
 
-            <Box mt="20" mb="20">
-                <img src="image/login_kakao.svg" alt="kakao"></img>
-                <img src="image/login_naver.svg" alt="naver"></img>
+            <Box mt="14" mb="14">
+                <Image mb="1" src="image/login_kakao.svg" alt="kakao login"></Image>
+                <Image mb="1" src="image/login_naver.svg" alt="naver login"></Image>
+                <Image mb="1" src="image/login_guest.svg" alt="guest login"></Image>
             </Box>
 
             <Box>
 
-            <Box fontSize="sm" style={{color:'#A8A8A8'}}>
+            <Box fontSize="sm" style={{color:'#A8A8A8'}} mb="24">
                 <Text>
                     로그인시 이거모찌의 서비스 약관,
                 </Text>
