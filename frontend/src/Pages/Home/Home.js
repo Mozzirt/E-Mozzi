5851c69cdeb13ee85adfb4913cbe8ce7e65eb15f
Hizini <ejfdnrl@naver.com>
2022-07-16 16:37:45 +0900

    [FE] 라우터 수정 컴포넌트 구조 수정 및 이름 변경 불필요 파일 삭제 sass, cross-env 설치

---
+++ frontend/src/Pages/Home/Home.js
@@ -0,0 +1,21 @@
+
+import React, { Component } from 'react'
+import './Home.scss'
+
+class Home extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <div className='home-root-container'>
+            </div>
+        )
+    }
+}
+
+export default Home
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,28 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import reportWebVitals from 'reportWebVitals'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import { ChakraProvider } from '@chakra-ui/react'
-import theme from 'design/index'
-import 'index.css'
-
-import Router from 'route/index'
-import App from 'App'
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
-  <ChakraProvider theme={theme}>
-    <React.StrictMode>
-      <Router>
-        <App />
-      </Router>
-    </React.StrictMode>
-  </ChakraProvider>
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals();
