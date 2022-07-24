f066692478fe9b8ff2850b82460ce8c2bd67dc09
joohwan812 <joohon812@nate.com>
2022-07-25 00:51:05 +0900

    [FE] 로그인 버튼 수정(box-shadow css 적용)

--- frontend/src/page/Login.js
+++ frontend/src/page/Login.js
@@ -1,19 +1,23 @@
 import { Box, Flex, Text, Image } from "@chakra-ui/react"
 
+import Join from "./Join"
 
 
 
 export default function Login() {
+
     return (
-        <Box align="center">
-            <Flex align="center" justify="center" h="400px" bg="gray.100">
-                <Image src="image/logo_main.svg" alt="main logo"></Image>
+        <Box h="100vh" align="center">
+            <Flex align="center" justify="center" h="450px" bg="gray.100">
+                <Image src="image/logo/main.svg" alt="main logo"></Image>
             </Flex>
 
+            <Join />
+
             <Box mt="14" mb="14">
-                <Image mb="1" src="image/login_kakao.svg" alt="kakao login"></Image>
-                <Image mb="1" src="image/login_naver.svg" alt="naver login"></Image>
-                <Image mb="1" src="image/login_guest.svg" alt="guest login"></Image>
+                <Image mb="1" src="image/login/kakao.svg" alt="kakao login"></Image>
+                <Image mb="1" src="image/login/naver.svg" alt="naver login"></Image>
+                <Image mb="1" src="image/login/guest.svg" alt="guest login"></Image>
             </Box>
 
             <Box>
