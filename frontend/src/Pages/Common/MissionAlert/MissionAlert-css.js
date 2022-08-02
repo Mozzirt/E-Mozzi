200562baebcfa3eef9fb8ff2053e6bb1eeb33dea
joohwan812 <joohon812@nate.com>
2022-08-03 07:59:59 +0900

    [FE] 미션 Alert 퍼블리싱 - TODO 1. 폰트 다운로드 2. 배경색 수정

---
+++ frontend/src/Pages/Common/MissionAlert/MissionAlert-css.js
@@ -0,0 +1,20 @@
+export const drawerContentCss = {
+  borderTopRadius: '24px',
+  backdropFilter: 'blur(20px)',
+  bg: 'inear-gradient(135deg, rgba(255, 255, 255, 0.21) 0%, #FFFFFF 2.07%, rgba(209, 252, 252, 0.21) 92.89%, #FFFFFF 95.89%, rgba(156, 247, 247, 0.12) 99.98%, rgba(62, 240, 240, 0) 99.99%)'
+}
+
+export const textCss = {
+  fontSize: '18px',
+  lineHeight: '26px',
+  textAlign: 'center',
+  fontFamily: 'Jost'
+}
+
+export const pointCss = {
+  fontSize: '13px',
+  lineHeight: '19px',
+  color: '#7F8187',
+  mt: '2px',
+  mb: '10.5px'
+}
--- frontend/src/Pages/Common/MissionAlert/MissionAlert.js
+++ frontend/src/Pages/Common/MissionAlert/MissionAlert.js
@@ -1,5 +1,7 @@
 import { Drawer, DrawerOverlay, DrawerContent, DrawerHeader, DrawerBody, HStack, Text, Stack, Input, Box, Button, useRadioGroup, Spacer, Image } from '@chakra-ui/react'
 
+import { textCss, pointCss } from './MissionAlert-css'
+
 export default function MissionAlert(props) {
 
     const { onClose, isOpen } = props
@@ -10,11 +12,17 @@ export default function MissionAlert(props) {
           <DrawerContent borderTopRadius="24px" backdropFilter="blur(20px)" bg="linear-gradient(135deg, rgba(255, 255, 255, 0.21) 0%, #FFFFFF 2.07%, rgba(209, 252, 252, 0.21) 92.89%, #FFFFFF 95.89%, rgba(156, 247, 247, 0.12) 99.98%, rgba(62, 240, 240, 0) 99.99%)">
               <DrawerHeader pt={'10px'}>
               </DrawerHeader>
-              <DrawerBody m={5} h="500px">
-                <Image src='images/MissionAlert/MissionComplete.svg'></Image>
+              <DrawerBody p="0px" mt="16px">
+                <Box h={159} ml="20%">
+                  <Image src='images/MissionAlert/MissionComplete.svg'></Image>
+                </Box>
+                <Box {...textCss}>
+                  <Text>히든목표 달성</Text>
+                  <Text {...pointCss}>+ 500 mp</Text>
+                </Box>
               </DrawerBody>
-              <Box>
-                  <Button color="white" h="54px" w="100%" bg="linear-gradient(122.69deg, rgba(126, 225, 246, 0.65) 22.21%, rgba(181, 163, 211, 0.65) 80.36%)">확인</Button>
+              <Box textAlign={'center'}>
+                  <Button color="white" h="55px" w="260px" mb='34px' bg="linear-gradient(220deg, #7EE1F6 -14.98%, #B5A3D3 60.27%)" borderRadius="38px">확인</Button>
               </Box>
           </DrawerContent>
       </Drawer>
